d67383d904aba78c73b026fb4c8307ef9ba66929
Fritz-2008 <fritzanimaciones@gmail.com>
2026-06-07 18:44:35 -0500

    Create Impares.java

---
+++ Impares.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+ 
+public class Impares {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 == 0) continue;
+
+            System.out.print(i + " ");
+        }
+    }
+}
